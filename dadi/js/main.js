18a92ae4df22ade5d871e53b328bd94b3ec1c5f5
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-07 17:08:06 +0200

    change puntata, add confirm

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -3,18 +3,18 @@
 //Se il numero scelto è superiore al numero scelto dal computer, ho vinto
 //Altrimenti vince il computer
 
-const puntata = parseInt(prompt('Inserisci un numero da 1 a 6'));
+const puntata = Math.floor(Math.random() * 6) + 1;
+const puntataComputer = Math.floor(Math.random() * 6) + 1;
+let genera = confirm('Benvenuto, confermare per generare un numero');
 
 if(puntata >= 1 && puntata <= 6){
 
-    const puntataComputer = Math.floor(Math.random() * 6) + 1;
-
     if(puntataComputer < puntata){
-        alert('Complimenti! Hai vinto!' + 'Scelta computer: ' + puntataComputer);
+        alert('Complimenti! Hai vinto!' + 'Scelta giocatore: ' + puntata + ' Scelta computer: ' + puntata );
     }else if (puntataComputer == puntata){
-        alert('Pareggio! ' + 'Scelta computer: ' + puntataComputer);      
+        alert('Pareggio! ' + 'Scelta giocatore: ' + puntata + ' Scelta computer: ' + puntata );     
     }else{
-        alert('Peccato! Ha vinto il computer! ' + 'Scelta computer: ' + puntataComputer);
+        alert('Peccato! Ha vinto il computer! ' + 'Scelta giocatore: ' + puntata + ' Scelta computer: ' + puntata );
     }
 }else{
     alert('Valore inserito non valido!');
